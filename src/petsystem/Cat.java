6cc9dada1571656812feaa7fa101b31fa064f013
samiraliyev <thesamiraliyev@mail.ru>
2026-01-31 11:26:14 +0500

    Initial commit PetAdoption project

---
+++ src/petsystem/Cat.java
@@ -0,0 +1,13 @@
+package petsystem;
+
+public class Cat extends Pet {
+
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String getType() {
+        return "Cat";
+    }
+}
--- src/petsystem/Main.java
+++ src/petsystem/Main.java
@@ -9,23 +9,62 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter dog name: ");
-        String name = sc.nextLine();
+        String dogName = sc.nextLine();
 
         System.out.print("Enter dog age: ");
-        int age = sc.nextInt();
+        int dogAge = sc.nextInt();
+        sc.nextLine();
 
-        Pet dog = new Dog(name, age);
+        Pet dog = new Dog(dogName, dogAge);
+
+        System.out.print("Enter cat name: ");
+        String catName = sc.nextLine();
+
+        System.out.print("Enter cat age: ");
+        int catAge = sc.nextInt();
+
+        Pet cat = new Cat(catName, catAge);
+
+        Shelter shelter = new Shelter();
+        shelter.addPet(dog);
+        shelter.addPet(cat);
+
+        System.out.println("\nAll pets in shelter:");
+        shelter.showPets();
+
+        System.out.println("\nOnly dogs:");
+        shelter.showDogs();
+
+        shelter.sortByName();
+        System.out.println("\nSorted pets:");
+        shelter.showPets();
+
+        Adopter adopter = new Adopter("John");
+
+        Pet found = shelter.findPet(catName);
+        adopter.adopt(found);
+
+        Pet test1 = new Dog("Rex", 3);
+        Pet test2 = new Dog("Rex", 3);
+
+        System.out.println("\nObject comparison:");
+        System.out.println(test1.equals(test2));
 
         PetDB.insertPet(dog);
+        PetDB.insertPet(cat);
+
+        System.out.println("\nPets from database:");
         PetDB.showPets();
 
-        System.out.print("Enter pet ID to update age: ");
+        System.out.print("\nEnter pet ID to update age: ");
         int id = sc.nextInt();
 
         System.out.print("Enter new age: ");
         int newAge = sc.nextInt();
 
         PetDB.updateAge(id, newAge);
+
+        System.out.println("\nUpdated database:");
         PetDB.showPets();
     }
 }
